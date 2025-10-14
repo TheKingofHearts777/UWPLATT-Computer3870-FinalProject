6a5ba7e577ba2de39af53ea8a745e07bafb30a49
TheKingofHearts777 <dbrodie678@gmail.com>
2025-10-14 19:50:13 +0000

    lower font size of img.alt text

--- poker-logic.js
+++ poker-logic.js
@@ -64,6 +64,7 @@ function renderCards(elemId, cards, hidden = false) {
             const img = document.createElement("img");
             img.src = "assets/card-back.jpg";
             img.alt = "Card Back";
+            img.style.fontSize = "10px";
             img.style.width = "100%";
             img.style.height = "100%";
             d.appendChild(img);
